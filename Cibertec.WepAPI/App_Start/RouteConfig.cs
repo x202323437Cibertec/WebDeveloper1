734045de6e937184383162bfc6a376b010895007
x202323437Cibertec <x202323437@cibertec.edu.pe>
2023-12-15 18:16:09 -0500

    Web API > iniciando las enseñanzas

---
+++ Cibertec.WepAPI/App_Start/RouteConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace Cibertec.WepAPI.App_Start
+{
+    public static class RouteConfig
+    {
+        public static void Register(HttpConfiguration pConfig)
+        {
+            pConfig.Routes.MapHttpRoute(
+                name: "DefaultApi"
+                ,routeTemplate: "api/{controller}/{id}"
+                ,defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
---
+++ Cibertec.WepAPI/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Cibertec.WepAPI.App_Start;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Cibertec.WepAPI.Startup))]
+
+namespace Cibertec.WepAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            RouteConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
